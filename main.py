55779de9bf80838434ca4aea685f6b549127f114
Keenan D. Buckley <keenandbuckley@protonmail.com>
2021-02-02 16:11:41 -0700

    Added code to send html email
    WIP currently sends email to the sender only. In the future email password needs to be fetched from an environment variable and recipients mailed based on plaintext mailing list.

--- main.py
+++ main.py
@@ -95,4 +95,23 @@ with open("menus.handlebars", "r") as template:
 
     with open("output.html", "w") as html:
         html.write(output)
+        
+# Prepare and send email to self
+port = 465  # For SSL
+smtp_server = "smtp.gmail.com"
+sender_email = "munchatmines@gmail.com"
+receiver_email = "munchatmines@gmail.com"
+password = input("Enter email password: ")
 
+message = MIMEMultipart('alternative')
+message['subject'] = 'Test Email'
+message['To'] = receiver_email
+message['From'] = sender_email
+
+html_body = MIMEText(output, 'html')
+message.attach(html_body)
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message.as_string())
